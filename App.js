29d8b8e11a939b4f53c0c833276ac5a98f3a70f7
Krishnaraj004 <142392358+Krishnaraj004@users.noreply.github.com>
2024-08-17 12:10:29 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,35 @@
+import React,{useEffect,useState} from "react";
+import ReactDom from 'react-dom'
+function App(){
+  const[users,setUsers]=useState([]);
+  const [loading,setLoding]=useState(true);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(response => response.json())
+    .then(data =>{
+      // console.log(data);
+      setUsers(data);
+      setLoding(false);
+    })  
+    .catch(error =>{
+      console.error("if it is not loading");
+      setLoding(false);
+    })
+  },[]);
+  // console.log(users[0])
+  if(loading){
+    return <p>Loading...</p>
+  }
+  return(
+    <div>
+      <ul>
+        <h1>User List</h1>
+        {users.map((user) =>(
+          <li key={user.id}>{user.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
